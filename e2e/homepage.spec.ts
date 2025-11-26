5f995a8354ba86645bb162df8204cd1d33e5d661
mrisoli <celorisoli@gmail.com>
2025-11-26 17:10:37 -0500

    fix e2e test

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -2,6 +2,6 @@ import { expect, test } from "@playwright/test";
 
 test("homepage loads successfully", async ({ page }) => {
   await page.goto("/");
-  const heading = page.locator("h1, h2");
-  await expect(heading).toBeVisible();
+  const heading = page.locator("h1");
+  await expect(heading).toHaveCount(1);
 });

b05da141a44ac64e9141ff5af633426648429e8f
Marcelo Risoli <celorisoli@gmail.com>
2026-02-13 20:04:34 +0100

    add vite-vercel-plugin

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,12 +2,19 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import vercel from "vite-plugin-vercel";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact()],
+	plugins: [
+		tsconfigPaths(),
+		tailwindcss(),
+		tanstackStart(),
+		viteReact(),
+		vercel(),
+	],
 	server: {
-		port: 3001,
+		port: (process.env.PORT as unknown as number) ?? 3001,
 	},
 	ssr: {
 		noExternal: ["@convex-dev/better-auth"],
